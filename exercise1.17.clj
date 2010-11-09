365de01e212c6a49b145a803271cc5fe30469413
Michael Patricios <michael.patricios@gmail.com>
2010-11-09 20:01:16 +0000

    Exercise 1.17 in clojure

--- exercise1.17.clj
+++ exercise1.17.clj
@@ -3,10 +3,23 @@
 ; 
 
 (defn mult [a b]
-  (if (= b 0)
+  (println a b)
+  (if (zero? b)
     0
-    (+ a (* a (- b 1)))))
+    (+ a (mult a (dec b)))))
     
-(defn fast-mul [ab]
-  ; TODO
-)
+; (mult 2 3)
+; (+ 2 (mult 2 2))
+; (+ 2 (+ 2 (mult 2 1)))
+; (+ 2 (+ 2 (+ 2 (mult 2 0))))
+; (+ 2 (+ 2 (+ 2 0)))
+; (+ 2 (+ 2 2))
+; (+ 2 4)
+; 6
+    
+(defn fast-mul [a b]
+  (loop [bl b sum 0]
+    (println a bl)
+    (if (zero? bl)
+      sum
+      (recur (dec bl) (+ sum a)))))
