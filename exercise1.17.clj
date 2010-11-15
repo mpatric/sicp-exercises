976ebe77a8a017571622699066deb1978372250e
Michael Patricios <michael.patricios@gmail.com>
2010-11-15 13:53:20 +0000

    Completed exercise 1.17 in clojure

--- exercise1.17.clj
+++ exercise1.17.clj
@@ -1,4 +1,14 @@
 ; Exercise 1.17
+;
+; Simple:
+; a*b = 0 if b is zero
+; a*b = a + (a*(b-1))
+;
+; Using double and halve:
+; a*b = double(a)*halve(b) if b is even
+; a*b = a + (a*(b-1)) if b is odd
+;
+; Note: these currently only work for a and b >= 0
 
 (defn mult [a b]
   (println a b)
@@ -16,8 +26,26 @@
 ; 6
     
 (defn fast-mult [a b]
-  (loop [bl b sum 0]
-    (println a bl)
+  (defn dbl [x]
+    (bit-shift-left x 1))
+  (defn hlv [x]
+    (bit-shift-right x 1))
+  (loop [al a bl b sum 0]
+    (println al bl sum)
     (if (zero? bl)
       sum
-      (recur (dec bl) (+ sum a)))))
+      (if (zero? (mod bl 2))
+        ; even
+        (recur (dbl al) (hlv bl) sum)
+        ; odd
+        (recur al (dec bl) (+ sum al))))))
+        
+; (fast-mult 2 3)
+; (loop 2 3 0)
+; (loop 2 2 2)
+; (loop 4 1 2)
+; (loop 4 0 6)
+; 6
+
+; (mult 12 12) -> 13 iterations
+; (fast-mult 12 12) -> 6 iterations
