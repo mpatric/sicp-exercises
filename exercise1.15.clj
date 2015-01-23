f8b2259c35482f496ad41109721e323bcb6283e1
Michael Patricios <michael.patricios@gmail.com>
2015-01-23 10:31:42 +0000

    Several exercises in scheme

--- exercise1.15.clj
+++ exercise1.15.clj
@@ -7,16 +7,16 @@
   (if (< x 0)
     (- 0 x)
     x))
-  
+
 (defn p [x]
   (println "CALL")
   (- (* 3 x) (* 4 (cube x))))
-  
+
 (defn sine [angle]
   (if (not (> (abs angle) 0.1))
     angle
     (p (sine (/ angle 3.0)))))
-    
+
 ; a. When calling (sine 12.15), p is called 5 times
 ; b. The order of growth of computation and space as a function of a for (sine a) both looks logarithmic:
 ;     O(log(a))
